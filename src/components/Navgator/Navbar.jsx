0ef6df977ce658830c661b23eee0484778f6c682
coder-Rit <001patilritesh@gmail.com>
2023-07-14 22:58:36 +0530

    redireciorFix

--- src/components/Navgator/Navbar.jsx
+++ src/components/Navgator/Navbar.jsx
@@ -1,31 +1,69 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./navbar.css";
-const Navbar = (props) => {
+import { useNavigate } from "react-router";
 
+const Navbar = (props) => {
+  const Navigate = useNavigate();
+ 
+
+  const directionHandler = (direction) => {
+    
+    Navigate(direction);
+  };
 
   return (
-     
-      
-      <nav className="navigation">
-        <ul >
-          <li>
-            <a href="/" className="nav-link" style={props.currentPage==="home"?{background:"rgb(136, 0, 255)",borderBottom: "2px solid #ffffff"}:{background:"#ffffff18"}}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/newTicket" className="nav-link"style={props.currentPage==="new"?{background:"rgb(136, 0, 255)",borderBottom: "2px solid #ffffff"}:{background:"#ffffff18"}}>
-              Create New Ticket
-            </a>
-          </li>
-          <li>
-            <a href="/auth" className="nav-link"style={props.currentPage==="auth"?{background:"rgb(136, 0, 255)",borderBottom: "2px solid #ffffff"}:{background:"#ffffff18"}}>
-              Login/Register
-            </a>
-          </li>
-        </ul>
-      </nav>
-     
+    <nav className="navigation">
+      <ul>
+        <li>
+          <a
+            onClick={() => directionHandler("/")}
+            className="nav-link"
+            style={
+              props.currentPage === "home"
+                ? {
+                    background: "rgb(136, 0, 255)",
+                    borderBottom: "2px solid #ffffff",
+                  }
+                : { background: "#ffffff18" }
+            }
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => directionHandler("/newTicket")}
+            className="nav-link"
+            style={
+              props.currentPage === "new"
+                ? {
+                    background: "rgb(136, 0, 255)",
+                    borderBottom: "2px solid #ffffff",
+                  }
+                : { background: "#ffffff18" }
+            }
+          >
+            Create New Ticket
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => directionHandler("/auth")}
+            className="nav-link"
+            style={
+              props.currentPage === "auth"
+                ? {
+                    background: "rgb(136, 0, 255)",
+                    borderBottom: "2px solid #ffffff",
+                  }
+                : { background: "#ffffff18" }
+            }
+          >
+            Login/Register
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
